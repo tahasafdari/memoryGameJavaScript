e4be10a9a1236aaa597b52e2c4d2d02488604870
tahasafdari <41860385+tahasafdari@users.noreply.github.com>
2022-03-12 20:24:30 +0200

    images
    images added

--- js/main.js
+++ js/main.js
@@ -27,11 +27,41 @@ const cardArray = [
         img: 'https://cdn.pixabay.com/photo/2012/04/13/01/51/hamburger-31775_960_720.png',
     },
     {
-        name: 'blank',
-        img: 'https://cdn.pixabay.com/photo/2016/06/02/02/33/triangles-1430105_960_720.png',
+        name:'pizza',
+        img: 'https://cdn.pixabay.com/photo/2012/04/01/16/51/pizza-23477_960_720.png',
+
     },
     {
-        name: 'white',
-        img: 'https://png.pngtree.com/thumb_back/fh260/background/20200821/pngtree-pure-white-minimalist-background-wallpaper-image_396581.jpg',
-    }
+        name: 'fries',
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/01/24/french-fries-155679_960_720.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'https://cdn.pixabay.com/photo/2012/04/14/15/57/drinks-34377_960_720.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'https://cdn.pixabay.com/photo/2020/03/19/07/19/ice-cream-4946596_960_720.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'https://cdn.pixabay.com/photo/2021/07/11/03/46/hot-dog-sandwich-6402792_960_720.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'https://cdn.pixabay.com/photo/2012/04/13/01/51/hamburger-31775_960_720.png',
+    },
 ]
+
+cardArray.sort(() => 0.5 - Math.random());
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard(){
+    for(let i = 0; i < 10; i++){
+      const card = document.createElement('img');
+      card.setAttribute('src', 'https://cdn.pixabay.com/photo/2016/06/02/02/33/triangles-1430105_960_720.png');
+      card.setAttribute('data-id', i)
+      gridDisplay.appendChild(card)
+    }
+}
+createBoard();
